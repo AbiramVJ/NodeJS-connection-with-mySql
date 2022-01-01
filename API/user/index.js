95c13e819e1671b5920223d8cebc03fad91acaba
V.Abiram <70306664+AbiramVJ@users.noreply.github.com>
2022-01-01 21:53:22 +0530

    CRUD Operations and APIs

--- API/user/index.js
+++ API/user/index.js
@@ -1,39 +1,114 @@
 import express from "express";
+
 const Router = express.Router();
 
+import path from "path";
+const app = express();
+
+//ejs
+const __dirname = path.resolve();
+app.set("view engine", "ejs");
+
 //database
-import connection from'../../config/connection.js';
+import db from "../../config/connection.js";
 
 /**
- * 
+ * METHOD: GET
+ * PARAMETER:NONE
+ * DESCRIPTION:GET A NEW CONTENT
+ */
+Router.get("/user", async (req, res) => {
+  try {
+    const [results] = await db.query("SELECT * FROM posts");
+    return res.render("home", { user: results });
+    //console.log(results);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
+
+
+/**
+ * METHOD: POST
+ * PARAMETER:NONE
+ * DESCRIPTION:POST A NEW CONTENT
+ */
+Router.post("/post", async (req, res) => {
+  try {
+    const data = [req.body.title, req.body.body, req.body.create_at];
+    console.log(data);
+    const newContent = await db.query(
+      "INSERT INTO posts(title,body,create_at) VALUE(?)",
+      [data]
+    );
+    return res.status(200).json({ user: "successfully added" });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
+
+/**
+ * METHOD:GET
+ * PARAMETER:ID
+ * DESCRIPTION: GETTING THE POST ACCORDING TO THE ID
+ *
  */
 
-Router.get("/user",async(req,res)=>{
+Router.get("/user/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    console.log(id);
+    const sql = `
+    SELECT * FROM posts WHERE id = ? 
+    `;
+    const [result] = await db.query(sql, [id]);
+    return res.json({ user: result });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
+
+/**
+ * METHOD:PUT
+ * PARAMETER:ID
+ * DESCRIPTION: UPDATE THE DATA ACCORDING TO THE ID
+ *
+ */
+
+Router.put("/user/:id/edit", async (req, res) => {
+  try {
+    const sql = `
+   UPDATE posts SET title='${req.body.title}',
+            body = '${req.body.body}' ,
+            create_at = '${req.body.create_at}'
+            WHERE id ='${req.params.id}'`;
+
+    const updateData = await db.query(sql);
+    return res.status(200).json({ content: "successfully added" });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
+
+Router.delete("/user/:id/delete",async(req,res)=>{
     try{
+        const {id}= req.params;
+        const findById = `SELECT * FROM posts WHERE id = ${req.params.id}`
+        const delateSql= `DELETE FROM posts WHERE id = ?`;
+
+        const [findRecord] = await db.query(findById);
+      
        
-        const Sql = "SELECT * FROM posts";  
-         connection.query(Sql,function(err,results){
-             //console.log(results);
-            if(err)
-             {
-                res.status(404).json({error:err.message});
-             }
-                 res.status(200).json({user:results});
-        });          
-    }
-    catch(error){
+        if(findRecord===null){
+            return res.status(200).json({delete:"no record found on this id "});
+        }else{
+            const [record]= await db.query(delateSql,[id]);
+            return res.status(200).json({delete:"record delete seccuscesfully",deletedRecord:findRecord});
+        }
+        
+
+    }catch(error){
         return res.status(500).json({error:error.message});
     }
 })
-
-// const sql = "SELECT * FROM posts";
-// await connection.execute(sql, function(err,result){
-//     if(err) throw err;
-//     console.log(result);
-// })
-
-
-
-
-
 export default Router;
--- index.js
+++ index.js
@@ -1,20 +1,30 @@
 import express from 'express';
+import path from "path";
 const app = express();
-//import cors from 'cors';
 
-//import mysql from 'mysql';
+//ejs
+const __dirname = path.resolve();
+app.set('view engine','ejs');
+
+
 
 // using the json transfer the data
 app.use(express.json());
 
+
+app.use(express.urlencoded({ extended: true })); // Parse incoming request bodies
+app.use(express.static('public')); // Serve static files (e.g. CSS files)
 
 //API
 import user from './API/user/index.js';
 
 //API
 app.use("/",user);
- 
 
+app.use((error,req,res,next)=>{
+  //console.log(error);
+  return res.json({error:error.message});
+})
 // set port, listen for requests
 const PORT = 3000;
 app.listen(PORT, () => {
